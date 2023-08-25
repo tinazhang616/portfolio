c1ae565956f255256b3c8eba384538042441ac66
Tina <tinazhang616@gmail.com>
2023-08-25 09:02:49 -0700

    ssh

--- src/portfolio.js
+++ src/portfolio.js
@@ -45,7 +45,7 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "VERSATILE FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
       "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
